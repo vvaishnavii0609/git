20288779f4d2dd1e79fd73952bc3f7a3b10ae94f
Vaishnavi <vaishnavinagwekar2018@gmail.com>
2026-06-17 02:59:51 +0530

    changes made

--- diff.java
+++ diff.java
@@ -1,4 +1,5 @@
 public class diff {
+  // BASIC JAVA FILE 
   public static void main(String[] args) {
     System.out.println("Hola");
   }
--- multiply.java
+++ multiply.java
@@ -2,6 +2,6 @@ public class multiply {
   public static void main(String[] args) {
     int a=5;
     int b=6;
-    System.out.println("The product is"+(a*b);
+    System.out.println("The product is"+(a*b));
   }
 }
